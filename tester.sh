7317fa4c93d18117e06e8758ca6fc02b2ed40ac8
Sergio Garcia <segarcia@student.42heilbronn.de>
2022-11-17 12:02:34 +0100

    ordering files

--- tester.sh
+++ tester.sh
@@ -1,7 +1,8 @@
 
 #!/bin/bash
+# To use this script we need to download this tester https://github.com/louisabricot/push_swap_tester
 for i in {1..500}
 do
 	echo "Testing: ${i}"
-	./tester_2/tester.sh ./ ${i} 3
+	./push_swap_tester/tester.sh ./ ${i} 3
 done
